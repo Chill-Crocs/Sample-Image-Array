0179155c34f964f04e0223ea6fb8f31e534b42b6
Eric Kim <kimericbyung@gmail.com>
2021-01-27 07:07:29 +0900

    Create image-url-array.js
    An array filled with image-urls to be used in seeding databases

---
+++ image-url-array.js
@@ -0,0 +1,22 @@
+const imageArr = [
+  https://homepages.cae.wisc.edu/~ece533/images/airplane.png,
+  https://homepages.cae.wisc.edu/~ece533/images/arctichare.png,
+  https://homepages.cae.wisc.edu/~ece533/images/baboon.png,
+  https://homepages.cae.wisc.edu/~ece533/images/barbara.png,
+  https://homepages.cae.wisc.edu/~ece533/images/boat.png,
+  https://homepages.cae.wisc.edu/~ece533/images/cat.png,
+  https://homepages.cae.wisc.edu/~ece533/images/fruits.png,
+  https://homepages.cae.wisc.edu/~ece533/images/frymire.png,
+  https://homepages.cae.wisc.edu/~ece533/images/girl.png,
+  https://homepages.cae.wisc.edu/~ece533/images/monarch.png,
+  https://homepages.cae.wisc.edu/~ece533/images/mountain.png,
+  https://homepages.cae.wisc.edu/~ece533/images/peppers.png,
+  https://homepages.cae.wisc.edu/~ece533/images/peppers.png,
+  https://homepages.cae.wisc.edu/~ece533/images/sails.png,
+  https://homepages.cae.wisc.edu/~ece533/images/serrano.png,
+  https://homepages.cae.wisc.edu/~ece533/images/tulips.png,
+  https://homepages.cae.wisc.edu/~ece533/images/watch.png,
+  https://homepages.cae.wisc.edu/~ece533/images/zelda.png,
+]
+
+export default imageArr;
